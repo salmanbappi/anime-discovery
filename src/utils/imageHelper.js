449c3ad64eb227deb0568783b976745198f1f509
User <user@example.com>
2025-12-22 21:08:33 +0600

    Remove image proxy

--- src/utils/imageHelper.js
+++ src/utils/imageHelper.js
@@ -1,5 +1,3 @@
 export const getProxiedImage = (url) => {
-  if (!url) return '';
-  // Using imageproxy.org as a reliable alternative
-  return `https://imageproxy.org/600x,q80,fit/${url}`;
+  return url || '';
 };
